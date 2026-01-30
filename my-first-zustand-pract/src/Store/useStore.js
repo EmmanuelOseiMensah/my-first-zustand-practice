85a548c09928b51fc4641fa7f06f521f599c463b
EmmanuelOseiMensah <accaoseimensah4christ32@gmail.com>
2026-01-30 21:06:44 +0000

    use persist zustand/middleware to keep data after refresh

--- my-first-zustand-pract/src/Store/useStore.js
+++ my-first-zustand-pract/src/Store/useStore.js
@@ -1,6 +1,10 @@
 import { create } from 'zustand'
+import {persist} from 'zustand/middleware'
 
-const useStore = create((set)=>(
+const useStore = create(
+  
+  persist(
+    (set)=>(
     {
   //the state
   count: 0, 
@@ -9,6 +13,10 @@ const useStore = create((set)=>(
   increment: ()=> set((state)=>({count:state.count + 1})),
   decrement: ()=> set((state)=>({count: state.count - 1})),
   reset: ()=>set({count: 0})
+    }), 
+    
+    {
+      name: 'counter-storage' //name of the item in the storage (local storage by default)
     }
 ))
 
